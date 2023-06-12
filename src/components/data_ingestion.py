4d76aff20d48f2b2e3c1f19047bb5126486cb699
UbaleSachin <ubalesachin22@gmail.com>
2023-06-12 21:04:53 +0530

    Data Ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -11,14 +11,15 @@ from dataclasses import dataclass
 
 
 @dataclass
-class Data_ingestion_config:
+class DataIngestionConfig:
     train_data_path: str = os.path.join('artifacts', 'train.csv')
     test_data_path: str = os.path.join('artifacts', 'test.csv')
     raw_data_path: str = os.path.join('artifacts', 'raw.csv')
 
+
 class DataIngestion:
     def __init__(self):
-        self.ingestion_config = Data_ingestion_config()
+        self.ingestion_config = DataIngestionConfig()
 
     def initiate_data_ingestion(self):
         logging.info('Data ingestion started')
---
+++ src/components/data_transformation.py
@@ -0,0 +1,7 @@
+import sys
+import os
+
+from sklearn.model_selection import train_test_split
+
+"""PYTHON PROGRAM FOR TRAINING AND TESTING"""
+
